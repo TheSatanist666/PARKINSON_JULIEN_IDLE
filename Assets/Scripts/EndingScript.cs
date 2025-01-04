51a72a359a590e08028b21f5b4de0089dc347b64
ESMA-JulienP <j.parkinson@rns.ecolescreatives.com>
2025-01-04 22:03:38 +0100

    father and mother voicelines added

---
+++ Assets/Scripts/EndingScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip _line1, _line2, _line3, _line4, _line5, _line6, _line7, _line8, _line9, line10, _line11;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
