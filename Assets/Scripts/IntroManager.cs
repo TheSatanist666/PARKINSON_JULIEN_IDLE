66c8b572d4efecb1ba3b087918e745a0a740973f
ESMA-JulienP <j.parkinson@rns.ecolescreatives.com>
2025-01-07 10:18:02 +0100

    menu finished

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _spirits;
+    [SerializeField]
+    private AudioClip _monologue;
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    public void LaunchIntro()
+    {
+        _spirits.SetActive(false); //masque les esprits du decors ainsi que le bouton
+        Intro();
+    }
+
+    private void Intro() //script different de LaunchIntro pour plus de clarte, il gere le monologue du personnage principal au debut
+    {
+        _audioSource.PlayOneShot(_monologue, 0.7F);
+        StartCoroutine(LaunchClicker());
+    }
+    private IEnumerator LaunchClicker()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1); //charge la scene principale du clicker
+    }
+}
